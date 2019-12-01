8d241d026439119e285f87e1a4798f9b896aefce
Andy Pearson <andy@londonparkour.com>
2019-12-01 09:42:06 +0000

    Initial Commit

---
+++ w3tc_stats_grafana.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: _ANDYP - W3 Total Cache - Stats to Grafana
+Plugin URI: http://londonparkour.com
+Description: Uses the filters on W3TC to output the stats for grafana.
+Version: 1.0
+Author: Andy Pearson
+Author URI: http://londonparkour.com
+*/
+
