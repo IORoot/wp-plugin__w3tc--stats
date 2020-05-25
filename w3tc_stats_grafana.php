ffbb454fabd210d6f527df1386947c8bab0bc594
Andy Pearson <andy@londonparkour.com>
2020-05-25 19:35:37 +0100

    Add emoji description

--- w3tc_stats_grafana.php
+++ w3tc_stats_grafana.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: _ANDYP - W3 Total Cache - Stats to Grafana
 Plugin URI: http://londonparkour.com
-Description: Uses the filters on W3TC to output the stats for grafana.
+Description: <strong>🧮METRICS</strong> | <em>Grafana</em> | Uses the filters on W3TC to output the stats for grafana.
 Version: 1.0
 Author: Andy Pearson
 Author URI: http://londonparkour.com
@@ -32,8 +32,10 @@ if (array_key_exists('triggerstats', $_GET)){
     $data = json_decode(get_site_option( 'w3tc_stats_history'));
 
     // Iterate over every entry of the array
+    // Overwriting each line - grok_exporter only takes the first REGEX hit
+    // And stores it in prometheus. 
     foreach ($data as $entry) {
-        $output .= json_encode($entry)."\n"; 
+        $output = json_encode($entry)."\n"; 
     }
 
     // output to the file.
